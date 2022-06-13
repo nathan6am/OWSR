9a4d2c3f2955ddae342f50d24e6a7168de99651a
nathan6am <nathanpho47@gmail.com>
2022-06-13 14:54:29 -0400

    added demo profile

--- components/modalContent/SignInContent.js
+++ components/modalContent/SignInContent.js
@@ -146,7 +146,7 @@ export default function SignInContent() {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
-                  email: "test@test.com",
+                  email: "test@test.test",
                   password: "123456",
                 }),
               });
