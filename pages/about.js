d994981b9c4b760ebb5bba1d8f8de491bead558e
nathan6am <nathanpho47@gmail.com>
2022-06-13 13:55:11 -0400

    deploymeny fixes

--- pages/about.js
+++ pages/about.js
@@ -11,10 +11,10 @@ export default function AboutPage() {
         <h2 className="text-xl mb-2">What is Lorem Ipsum?</h2>
         <p className="mb-4">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
+          industry. Lorem Ipsum has been the industry&aposs standard dummy text
+          ever since the 1500s, when an unknown printer took a galley of type
+          and scrambled it to make a type specimen book. It has survived not
+          only five centuries, but also the leap into electronic typesetting,
           remaining essentially unchanged. It was popularised in the 1960s with
           the release of Letraset sheets containing Lorem Ipsum passages, and
           more recently with desktop publishing software like Aldus PageMaker
@@ -25,12 +25,13 @@ export default function AboutPage() {
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. The point of
           using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here, content here', making it
-          look like readable English. Many desktop publishing packages and web
-          page editors now use Lorem Ipsum as their default model text, and a
-          search for 'lorem ipsum' will uncover many web sites still in their
-          infancy. Various versions have evolved over the years, sometimes by
-          accident, sometimes on purpose (injected humour and the like).
+          letters, as opposed to using &aposContent here, content here&apos,
+          making it look like readable English. Many desktop publishing packages
+          and web page editors now use Lorem Ipsum as their default model text,
+          and a search for &aposlorem ipsum&apos will uncover many web sites
+          still in their infancy. Various versions have evolved over the years,
+          sometimes by accident, sometimes on purpose (injected humour and the
+          like).
         </p>
       </section>
     </div>
